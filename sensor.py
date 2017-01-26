6f9135a30a6c6c8faa32ee4a5a50643831d29f1b
JohanSundman <johan00sundman@gmail.com>
2017-01-26 10:21:02 +0100

    Final threading problem solution?

--- sensor.py
+++ sensor.py
@@ -4,6 +4,9 @@ import imu # Gyro & accelerometer
 
 class Sensor(threading.Thread):
 	def __init__(self):
+		# Make sure the parent's init gets a call
+		threading.Thread.__init__(self)
+		
 		# Create the Imu object
 		self.sensor = imu.Imu()
 
